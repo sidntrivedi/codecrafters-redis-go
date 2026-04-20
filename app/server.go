f0ef259102def2c984224bacd1c843934b88f993
Siddhant N Trivedi <phoenix@Siddhants-MacBook-Pro.local>
2026-04-20 11:11:03 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -15,26 +15,28 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	// Read message from the connection and check if its PING.
-	data := make([]byte, 1024) // 1 KB buffer.
-	n, err := conn.Read(data)
-	if err != nil {
-		fmt.Println("Error reading message from connection: ", err.Error())
-		os.Exit(1)
-	}
+		// Read message from the connection and check if its PING.
+		data := make([]byte, 1024) // 1 KB buffer.
+		n, err := conn.Read(data)
+		if err != nil {
+			fmt.Println("Error reading message from connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	message := data[:n]
-	fmt.Println(message)
+		message := data[:n]
+		fmt.Println(message)
 
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Error writing message into connection: ", err.Error())
-		os.Exit(1)
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Error writing message into connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
 }
