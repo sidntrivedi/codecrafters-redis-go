d647b18f16abeb54796c133f2f3a6fd3f3855222
Siddhant N Trivedi <phoenix@Siddhants-MacBook-Pro.local>
2026-04-20 11:05:52 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -10,14 +10,31 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-    l, err := net.Listen("tcp", "0.0.0.0:6379")
-    if err != nil {
-        fmt.Println("Failed to bind to port 6379")
-        os.Exit(1)
-    }
-    _, err = l.Accept()
-    if err != nil {
-        fmt.Println("Error accepting connection: ", err.Error())
-        os.Exit(1)
-    }
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	// Read message from the connection and check if its PING.
+	data := make([]byte, 1024) // 1 KB buffer.
+	n, err := conn.Read(data)
+	if err != nil {
+		fmt.Println("Error reading message from connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	message := data[:n]
+	fmt.Println(message)
+
+	_, err = conn.Write([]byte("PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error writing message into connection: ", err.Error())
+		os.Exit(1)
+	}
 }
