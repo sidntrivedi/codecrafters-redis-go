d1ed2e4fb596de4e384889ecfb227b06b92bc253
Siddhant N Trivedi <phoenix@Siddhants-MacBook-Air.local>
2026-04-30 18:16:03 +0530

    codecrafters submit [skip ci]

--- app/watch.go
+++ app/watch.go
@@ -1,6 +1,9 @@
 package main
 
 func (s *Server) handleWATCHCmd(client *Client, message []string) (string, error) {
+	if client.queueCmds {
+		return "-ERR WATCH inside MULTI is not allowed\r\n", nil
+	}
 	return "+OK\r\n", nil
 
 }
